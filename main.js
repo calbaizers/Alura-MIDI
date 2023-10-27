84ad2ed26ac1888e10eac41ebb7c7f185a4b1e1a
calbaizers <107188899+calbaizers@users.noreply.github.com>
2023-10-27 08:34:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play('');
+function tocaSom(idElementoAudio){
+   const elemento = document.querySelector(seletorAudio).play('');
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    }else{
+        alert('Elemento não encontrado')
+    }
+const listaDeTeclas = document.querySeclectorAll('.tecla');
 
+for (contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const instrumento = listaDeTeclas[contador].classList[1];
+
+    const tecla = listaDeTeclas[contador];
+    const idAudio = `#son_${instrumento}`;
+        tecla.onclick = function(){
+            tocaSom(idAudio);
+        };
+
+        tecla.onkeydown = function (){
+            if(evento.code === 'Space'|| evento.code === 'Enter'){
+                tecla.classList.add('ativa');
+            }
+           } 
+        }
+        tecla.onkeyup = function(){
+            tecla.classList.remove('ativa');
+        }
+    
+
+listaDeTeclas[1].onclick = tocaSomClap;
+listaDeTeclas[2].onclick = tocaSomTim;
+listaDeTeclas[3].onclick = tocaSomPuff;
